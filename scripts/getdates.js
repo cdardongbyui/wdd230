8aaa8698bd2ccf8b6f18df527714504042a134cf
cdardongbyui <cdardong.byui@gmail.com>
2023-09-17 18:49:23 -0600

    Fixing url github

---
+++ scripts/getdates.js
@@ -0,0 +1,9 @@
+
+
+document.getElementById("year").innerHTML = new Date().getFullYear();
+
+Name = "Cristian Joaquin Dardon Godoy";
+
+document.getElementById("name-footer").innerHTML = Name;
+
+document.getElementById("last-modified").innerHTML = "Last Modification  " + document.lastModified;
