68e741870a5a176e68b82da9102480e12169dc8c
cdardongbyui <cdardong.byui@gmail.com>
2023-09-23 10:01:02 -0600

    Principles design

---
+++ lesson02/scripts/design-principles.js
@@ -0,0 +1,8 @@
+
+Name = "Cristian Joaquin Dardon Godoy";
+
+document.getElementById("year").innerHTML = new Date().getFullYear() +" | ";
+document.getElementById("owner").innerHTML =  Name +" | ";
+
+
+document.getElementById("update").innerHTML = " Last Modification  " + document.lastModified;
