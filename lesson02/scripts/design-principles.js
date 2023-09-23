d63f9b3943853eebdff81433dd7fdee385f83efb
cdardongbyui <cdardong.byui@gmail.com>
2023-09-23 10:06:07 -0600

    Fixing footers

--- lesson02/scripts/design-principles.js
+++ lesson02/scripts/design-principles.js
@@ -2,7 +2,7 @@
 Name = "Cristian Joaquin Dardon Godoy";
 
 document.getElementById("year").innerHTML = new Date().getFullYear() +" | ";
-document.getElementById("owner").innerHTML =  Name +" | ";
+document.getElementById("owner").innerHTML =  Name +"  | ";
 
 
-document.getElementById("update").innerHTML = " Last Modification  " + document.lastModified;
+document.getElementById("update").innerHTML = "  Last Modification  " + document.lastModified;
