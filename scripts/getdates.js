5192fb3b5c5ddb1c4bc72782f2801ef238ac5da1
cdardongbyui <cdardong.byui@gmail.com>
2023-09-28 11:51:47 -0600

    Finishig home page

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,4 +1,11 @@
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
 
+hamButton.addEventListener('click', () => {
+    hamButton.classList.toggle('open');
+	navigation.classList.toggle('open');
+	
+});
 
 document.getElementById("year").innerHTML = new Date().getFullYear();
 
@@ -7,3 +14,4 @@ Name = "Cristian Joaquin Dardon Godoy";
 document.getElementById("name-footer").innerHTML = Name;
 
 document.getElementById("last-modified").innerHTML = "Last Modification  " + document.lastModified;
+
